2d06f14fbe8f631b7abc274f16e22f089eca61ea
Joshua Loran <joshua.loran@gmail.com>
2020-10-02 17:20:52 -0700

    Updates

---
+++ precache-manifest.a4287018c0c30dc93ea5a8cda83ac8ab.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d035b19c29aeeed8a6a56818f837373",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "c6e529c5deccf619c9a4",
+    "url": "/Portfolio/static/css/2.04ff9426.chunk.css"
+  },
+  {
+    "revision": "8e194472fcb904cfce03",
+    "url": "/Portfolio/static/css/main.af264abf.chunk.css"
+  },
+  {
+    "revision": "c6e529c5deccf619c9a4",
+    "url": "/Portfolio/static/js/2.52307994.chunk.js"
+  },
+  {
+    "revision": "572bfeb62474f0cec44e259ca5a99f18",
+    "url": "/Portfolio/static/js/2.52307994.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e194472fcb904cfce03",
+    "url": "/Portfolio/static/js/main.7bc7c1a8.chunk.js"
+  },
+  {
+    "revision": "b83a37198ba343383876",
+    "url": "/Portfolio/static/js/runtime-main.e440ee5a.js"
+  },
+  {
+    "revision": "e97f5a3544fc2b4cd59f278a8696bc0b",
+    "url": "/Portfolio/static/media/ConmitScreen.e97f5a35.png"
+  },
+  {
+    "revision": "0ee38c16b08f4489c8aa10dfc6c62c6a",
+    "url": "/Portfolio/static/media/ProfilePic.0ee38c16.jpeg"
+  },
+  {
+    "revision": "3b593ed554e4845532b13633e3bd057d",
+    "url": "/Portfolio/static/media/conmittoLogo.3b593ed5.png"
+  },
+  {
+    "revision": "778a580f37e318a87169db71b078dfc6",
+    "url": "/Portfolio/static/media/farmfrenzy.778a580f.gif"
+  },
+  {
+    "revision": "af01ccda82a1b2c3ff74a87dcdc66591",
+    "url": "/Portfolio/static/media/homeControlConnectScreen.af01ccda.png"
+  },
+  {
+    "revision": "8df849bb8eff45d72ac1fad7177406ff",
+    "url": "/Portfolio/static/media/homeControlHomeScreen.8df849bb.png"
+  },
+  {
+    "revision": "d4c0dde34d0b3e67b5e35f2d963ab64e",
+    "url": "/Portfolio/static/media/homeControlScreen.d4c0dde3.png"
+  },
+  {
+    "revision": "26953762721a2b17b7119139113894a2",
+    "url": "/Portfolio/static/media/socialWildernessDestinationScreen.26953762.png"
+  },
+  {
+    "revision": "696f2ee2e7bb53ea6c5c5fd88f149f63",
+    "url": "/Portfolio/static/media/socialWildernessLeoScreen.696f2ee2.png"
+  },
+  {
+    "revision": "b846cb9d41c4f02863286134f1cd995b",
+    "url": "/Portfolio/static/media/socialWildernessScreen.b846cb9d.png"
+  },
+  {
+    "revision": "46326b5a044929d332d9dfd00f4a505a",
+    "url": "/Portfolio/static/media/sylvan.46326b5a.png"
+  },
+  {
+    "revision": "0ed15415422750d6a599a2a3fdf81931",
+    "url": "/Portfolio/static/media/sylvanScreen.0ed15415.png"
+  }
+]);
