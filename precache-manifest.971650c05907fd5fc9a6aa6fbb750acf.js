5193123f082c6cacca1228140fe85db89b50a2b9
Joshua Loran <joshua.loran@gmail.com>
2020-10-03 10:39:48 -0700

    Updates

---
+++ precache-manifest.971650c05907fd5fc9a6aa6fbb750acf.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e6d65e6bfdedaf8ebcc0579b62c1c70",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5b8c202b1fe00e11900",
+    "url": "/static/css/2.04ff9426.chunk.css"
+  },
+  {
+    "revision": "1757cfe12863b4147375",
+    "url": "/static/css/main.af264abf.chunk.css"
+  },
+  {
+    "revision": "c5b8c202b1fe00e11900",
+    "url": "/static/js/2.cb275122.chunk.js"
+  },
+  {
+    "revision": "572bfeb62474f0cec44e259ca5a99f18",
+    "url": "/static/js/2.cb275122.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1757cfe12863b4147375",
+    "url": "/static/js/main.b74ef067.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "e97f5a3544fc2b4cd59f278a8696bc0b",
+    "url": "/static/media/ConmitScreen.e97f5a35.png"
+  },
+  {
+    "revision": "dbe145092e16e4049ee435d55c372025",
+    "url": "/static/media/JoshuaLoranResume.dbe14509.pdf"
+  },
+  {
+    "revision": "0ee38c16b08f4489c8aa10dfc6c62c6a",
+    "url": "/static/media/ProfilePic.0ee38c16.jpeg"
+  },
+  {
+    "revision": "3b593ed554e4845532b13633e3bd057d",
+    "url": "/static/media/conmittoLogo.3b593ed5.png"
+  },
+  {
+    "revision": "778a580f37e318a87169db71b078dfc6",
+    "url": "/static/media/farmfrenzy.778a580f.gif"
+  },
+  {
+    "revision": "af01ccda82a1b2c3ff74a87dcdc66591",
+    "url": "/static/media/homeControlConnectScreen.af01ccda.png"
+  },
+  {
+    "revision": "8df849bb8eff45d72ac1fad7177406ff",
+    "url": "/static/media/homeControlHomeScreen.8df849bb.png"
+  },
+  {
+    "revision": "d4c0dde34d0b3e67b5e35f2d963ab64e",
+    "url": "/static/media/homeControlScreen.d4c0dde3.png"
+  },
+  {
+    "revision": "26953762721a2b17b7119139113894a2",
+    "url": "/static/media/socialWildernessDestinationScreen.26953762.png"
+  },
+  {
+    "revision": "696f2ee2e7bb53ea6c5c5fd88f149f63",
+    "url": "/static/media/socialWildernessLeoScreen.696f2ee2.png"
+  },
+  {
+    "revision": "b846cb9d41c4f02863286134f1cd995b",
+    "url": "/static/media/socialWildernessScreen.b846cb9d.png"
+  },
+  {
+    "revision": "46326b5a044929d332d9dfd00f4a505a",
+    "url": "/static/media/sylvan.46326b5a.png"
+  },
+  {
+    "revision": "0ed15415422750d6a599a2a3fdf81931",
+    "url": "/static/media/sylvanScreen.0ed15415.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Portfolio/precache-manifest.a4287018c0c30dc93ea5a8cda83ac8ab.js"
+  "/precache-manifest.971650c05907fd5fc9a6aa6fbb750acf.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
