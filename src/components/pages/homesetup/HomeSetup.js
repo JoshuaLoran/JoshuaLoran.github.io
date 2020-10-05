c6264569a67398779132a01500e73f51dcceba2c
Joshua Loran <joshua.loran@gmail.com>
2020-10-05 09:25:47 -0700

    add under construction

--- src/components/pages/homesetup/HomeSetup.js
+++ src/components/pages/homesetup/HomeSetup.js
@@ -1,7 +1,11 @@
 import React from "react";
+import {Text, Flex} from "@chakra-ui/core";
 
 const HomeSetup = () => {
-  return <div>HOME SETUP</div>;
+  return <Flex direction="column">
+    <Text fontSize={["20px", "30px", "38px", "48px"]}>HOME SETUP</Text>
+    <Text fontSize={["16px", "24px", "30px", "34px"]}>COMING SOON!</Text>
+  </Flex>;
 };
 
 export {HomeSetup};
