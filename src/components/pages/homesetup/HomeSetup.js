885cd64ead907caa684328b4f363327bee71df1e
Joshua Loran <joshua.loran@gmail.com>
2020-10-06 15:11:44 -0700

    add coming soon

--- src/components/pages/homesetup/HomeSetup.js
+++ src/components/pages/homesetup/HomeSetup.js
@@ -4,7 +4,7 @@ import {Text, Flex} from "@chakra-ui/core";
 const HomeSetup = () => {
   return <Flex direction="column">
     <Text fontSize={["20px", "30px", "38px", "48px"]}>HOME SETUP</Text>
-    <Text fontSize={["16px", "24px", "30px", "34px"]}>COMING SOON!</Text>
+    <Text fontSize={["16px", "24px", "30px", "34px"]}git >COMING SOON!</Text>
   </Flex>;
 };
 
